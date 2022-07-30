60aedd649867e4d85b94e7d29c3b27574b12a0a0
Andrii Biakov <an.bjakov@gmail.com>
2022-07-30 22:05:06 +0300

    'inital'

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict"
+
+let userName = prompt("Your name?");
+alert(userName);
+
+let firstVar = 2;
+let secondVar = 0;
+
+let varDifference = firstVar - secondVar;
+alert(firstVar+" минус "+secondVar+" = "+ varDifference);
+
+let varSum = firstVar + secondVar;
+alert(firstVar+" плюс "+secondVar+" = "+varSum);
+
+let varMultiplication = firstVar*secondVar;
+alert(firstVar+" умножить "+secondVar+" = "+varMultiplication);
+
+let varDivided = firstVar/secondVar;
+alert(firstVar+" делить "+secondVar+" = "+varDivided);
+
+let varPower = firstVar ** secondVar;
+alert(firstVar+" в степени "+secondVar+" = "+varPower);
+
