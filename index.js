a6d201648d8119b678a7d4cae4bcb1383faeada9
Andrii Biakov <an.bjakov@gmail.com>
2022-07-30 22:16:54 +0300

    changed variables names

--- index.js
+++ index.js
@@ -3,21 +3,21 @@
 let userName = prompt("Your name?");
 alert(userName);
 
-let firstVar = 2;
-let secondVar = 0;
+let a = 2;
+let b = 0;
 
-let varDifference = firstVar - secondVar;
-alert(firstVar+" минус "+secondVar+" = "+ varDifference);
+let differenceResult = a - b;
+alert(`${a} минус ${b} =  ${differenceResult}`);
 
-let varSum = firstVar + secondVar;
-alert(firstVar+" плюс "+secondVar+" = "+varSum);
+let sumResult = a + b;
+alert(`${a}+ плюс b = ${sumResult}`);
 
-let varMultiplication = firstVar*secondVar;
-alert(firstVar+" умножить "+secondVar+" = "+varMultiplication);
+let multiplicationResult = a*b;
+alert(a+" умножить "+b+" = "+multiplicationResult);
 
-let varDivided = firstVar/secondVar;
-alert(firstVar+" делить "+secondVar+" = "+varDivided);
+let dividedResult = a/b;
+alert(a+" делить "+b+" = "+multiplicationResult);
 
-let varPower = firstVar ** secondVar;
-alert(firstVar+" в степени "+secondVar+" = "+varPower);
+let powerResult = a ** b;
+alert(a+" в степени "+b+" = "+powerResult);
 
